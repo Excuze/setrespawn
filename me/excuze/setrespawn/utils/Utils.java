b5cea4cd1ee096cf77546e0071ef2545044f96e2
Excuze <62854354+Excuze@users.noreply.github.com>
2020-09-09 19:31:25 -0400

    Add files via upload
    Posted.

---
+++ me/excuze/setrespawn/utils/Utils.java
@@ -0,0 +1,17 @@
+/*   */ package me.excuze.setrespawn.utils;
+/*   */ 
+/*   */ import org.bukkit.ChatColor;
+/*   */ 
+/*   */ 
+/*   */ public class Utils
+/*   */ {
+/*   */   public static String chat(String s) {
+/* 9 */     return ChatColor.translateAlternateColorCodes('&', s);
+/*   */   }
+/*   */ }
+
+
+/* Location:              C:\Users\Tobia\Documents\Plugin Exports\SetRespawn.jar!\me\excuze\setrespaw\\utils\Utils.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
